fbb780b02ba6403f5e553ab3c74a79a8fcd9a93d
NodeReplacer <80176553+NodeReplacer@users.noreply.github.com>
2022-03-20 20:02:24 -0400

    Changing control types
    Now that I know it works I have to swap control types from using the keyboard to casting a ray to the mouse position from the camera.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,6 +7,7 @@ public class GameController : MonoBehaviour
     public List<Transform> turnQueue = new List<Transform>();
     public List<Transform> tiles = new List<Transform>();
     
+    Ray TouchRay => Camera.main.ScreenPointToRay(Input.mousePosition);
     
     // Start is called before the first frame update
     void Start() {
@@ -18,6 +19,12 @@ public class GameController : MonoBehaviour
     }
     // Update is called once per frame
     void Update() {
-        
+        if (Input.GetMouseButtonDown(0)) {
+            HandleClick();
+        }
+    }
+    
+    void HandleClick() {
+        //Get the tile that was clicked on.
     }
 }
